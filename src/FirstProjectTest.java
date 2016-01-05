6b12344c829c34eda25600b9a318b826bbef5265
Anil Kr Sharma <er.anilkrsharma@gmail.com>
2016-01-05 14:04:48 +0530

    Again Hello world

--- src/FirstProjectTest.java
+++ src/FirstProjectTest.java
@@ -4,7 +4,7 @@ public class FirstProjectTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello The fucking world");
+		System.out.println("Hellooos... The fucking world");
 	}
 
 }
